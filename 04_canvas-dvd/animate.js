91882d7b6e03b0f7a9d2ce32d646dc86ad883960
unknown <rlin6@stuy.edu>
2019-02-06 22:46:09 -0500

    k04

--- 04_canvas-dvd/animate.js
+++ 04_canvas-dvd/animate.js
@@ -9,87 +9,89 @@
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
 
-//initializing animation variables
+//initializing dot animation variables
 var radius = 0;
 var requestID = 0;
 var max = c.width / 2;
-var animating; //are we stopped or animating?
-var growing; //are we growing or shrinking?
-
-//dvd variables
-var up;
-var right;
+var growing = true; //are we growing or shrinking?
+var dvd; 
 
 //clear canvas function
-var clear = function(){
+var clear = function(e){
     ctx.clearRect(0, 0, c.width, c.height);
 }
 
 //stops animation
 var pause = function(e) {
-    if (animating) {
-	window.cancelAnimationFrame(requestID);
-	animating = false;
-    }
+    window.cancelAnimationFrame(requestID);
+    requestID = 0;
 };
 
 //makes animation
-var play = function(e) {
+var play = function() {
+
     window.cancelAnimationFrame(requestID);
-    //first press
-    if (animating == null) {
-	animating = true;
-	growing = true;
+    clear();
+
+    ctx.beginPath();
+    ctx.fillStyle = '#A491D3'
+    ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
+    ctx.fill();
+    if (growing) radius += 1;
+    else {
+        radius -= 1;
+    }
+    if (radius == max) {
+        growing = false;
+    }
+    if (radius == 0) {
+        growing = true;
     }
 
-    if (! animating) {
-	animating = true;
-    }
-
-    if (animating) {
-	requestID = window.requestAnimationFrame(play);
-	ctx.beginPath();
-	ctx.fillStyle = '#A491D3'
-	clear();
-	ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
-	ctx.fill();
-	if (growing) radius += 1;
-	else {
-	    radius -= 1;
-	}
-
-	if (radius == max) {
-	    growing = false;
-	}
-
-	if (radius == 0) {
-	    growing = true;
-	}
-    }
+    requestID = window.requestAnimationFrame(play);
+    console.log(requestID);
 };
 
 var dvdLogoSetup = function() {
+    
     window.cancelAnimationFrame(requestID);
-    clear();
+
+    //variables for dvd animation
     var rectWidth = 100;
     var rectHeight = 50;
 
-    var rectX = Math.floor( Math.random() * (c.width-rectWidth));
-    var rectY = Math.floor( Math.random() * (c.height-rectHeight));
+    var rectX = Math.floor( Math.random() * (c.width-rectWidth));  //rand starting x
+    var rectY = Math.floor( Math.random() * (c.height-rectHeight)); //rand starting y
 
-    var xVel = 1;
-    var yVel = 1;
+    var xVel = 1;  //x velocity
+    var yVel = 1;  //y velocity
 
     var logo = new Image();
     logo.src = "logo_dvd.jpg";
 
-    var dvdLogo = function() {
-	ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
-	
-	requestId = window.requestAnimationFrame(dvdLogo);
-    };
-    dvdLogo();
+    var animateDVD = function() {
+        window.cancelAnimationFrame(requestID);
+        clear();
+    
+        ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
+    
+        if (rectX <= 0 || rectX >= c.width - rectWidth) {
+            xVel *= -1;
+        }
+    
+        if (rectY <= 0 || rectY >= c.height - rectHeight) {
+            yVel *= -1;
+        }
+    
+        rectX += xVel;
+        rectY += yVel;
+    
+        requestId = window.requestAnimationFrame(animateDVD);
 
+        console.log(requestID);
+    }
+
+    animateDVD();
 }    
 
 //grabbing button stuff
@@ -100,4 +102,4 @@ var dvd = document.getElementById("dvd");
 //assigning events to buttions
 stop.addEventListener("click", pause);
 circle.addEventListener("click", play);
-dvd.addEventListener("click", dvdLogoSetup)
+dvd.addEventListener("click", dvdLogoSetup);
